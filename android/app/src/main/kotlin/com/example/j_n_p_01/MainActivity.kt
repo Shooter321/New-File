249c7242f79614f1fbcc0f7cb9f96ec458860301
Anis Ur Rehman <anisurrehman069@gmail.com>
2024-03-03 15:35:35 +0500

    cordwithanis

---
+++ android/app/src/main/kotlin/com/example/j_n_p_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.j_n_p_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+void main(){
+
+  var name = 'cordwithanis';
+  print (name);
+
+
+  int name1 = 5;
+  print(name1);
+
+  double name2 = 5;
+  print(name2);
+
+  num name3 = 5.25;
+  print(name3);
+
+  // bool islogin = true;
+  // print(islogin);
+  // bool notlogin = false;
+  // print(notlogin);
+
+
+
+
+    int num1 = 021;
+    int num2 = 264;
+    print (num1+num2);
+
+
+    double num3 = 0.54;
+    double num4 = 5.89;
+    print (num3 -num4);
+
+    num num5 = 20;
+    num num6 = 5;
+    print (num5 ~/num6);
+
+
+
+    int num7 = 20;
+    int num8 = 5;
+    int num9 = 20;
+    int num10 = 5;
+    print (num7 == num8 && num9 == num10 );
+
+
+  int num11= 20;
+  int num12= 5;
+  int num13 = 20;
+  int num14 = 5;
+  print (num11 == num12 || num13 == num14);
+
+//Git Commands
+
+  //git init
+  //git add.
+  //git commint -m "any msg"
+  //git remote addd origin
+  //http://github.com/anis/abc.git
+  //git push u- origin master
+
+
+
+
+
+
+  }
